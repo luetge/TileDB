4cb9f4d13b2379cb9920c8bfecc2821e14d874d7
Tyler Denniston <tyler@tiledb.io>
2018-08-08 17:07:55 -0400

    script

--- test/benchmarking/benchmark.py
+++ test/benchmarking/benchmark.py
@@ -10,6 +10,28 @@ import sys
 benchmark_src_dir = os.path.abspath('src')
 benchmark_build_dir = os.path.abspath('build')
 
+if os.name == 'posix':
+    if sys.platform == 'darwin':
+        os_name = 'mac'
+        libtiledb_name = 'libtiledb.dylib'
+    else:
+        os_name = 'linux'
+        libtiledb_name = 'libtiledb.so'
+elif os.name == 'nt':
+    os_name = 'windows'
+    libtiledb_name = 'tiledb.dll'
+
+
+def drop_fs_caches():
+    """Drops local filesystem caches."""
+    if os_name == 'linux':
+        subprocess.call(
+            ['sudo', 'sh', '-c', 'echo 3 >/proc/sys/vm/drop_caches'])
+    elif os_name == 'mac':
+        subprocess.call(['sudo', 'purge'])
+    else:
+        print('WARNING: FS cache drop unimplemented')
+
 
 def find_tiledb_path(args):
     """
@@ -23,33 +45,44 @@ def find_tiledb_path(args):
         dirs.append(os.environ['TILEDB_PATH'])
     for d in dirs:
         libdir = os.path.join(d, 'lib')
-        if os.path.exists(os.path.join(libdir, 'libtiledb.dylib')):
+        if os.path.exists(os.path.join(libdir, libtiledb_name)):
             return os.path.abspath(d)
     return None
 
 
-def get_all_benchmarks():
-    pass
-
+def list_benchmarks(show=False):
+    """Returns a list of benchmarks."""
+    executables = glob.glob(os.path.join(benchmark_build_dir, 'bench_*'))
+    if show:
+        print('{} benchmarks:'.format(len(executables)))
+    names = []
+    for exe in sorted(executables):
+        name = os.path.basename(exe)
+        names.append(name)
+        if show:
+            print('  {}'.format(name))
+    return names
 
 
 def build_benchmark(args):
+    """Builds all the benchmark programs."""
     if not os.path.exists(benchmark_build_dir):
         os.mkdir(benchmark_build_dir)
     tiledb_path = find_tiledb_path(args)
-    print('Building benchmark program...')
     subprocess.check_output(
         ['cmake', '-DCMAKE_PREFIX_PATH={}'.format(tiledb_path),
          benchmark_src_dir],
         cwd=benchmark_build_dir)
     subprocess.check_output(['make', '-j4'], cwd=benchmark_build_dir)
-    print('Done.')
 
 
 def run_benchmark(args):
-    exe = os.path.join(benchmark_build_dir, 'benchmark')
-    output_json = subprocess.check_output([exe])
-    results = json.loads(output_json)
+    """Runs the benchmark programs."""
+    benchmarks = list_benchmarks()
+    for b in benchmarks:
+        exe = os.path.join(benchmark_build_dir, b)
+        output_json = subprocess.check_output([exe])
+        # results = json.loads(output_json)
 
 
 def main():
@@ -58,6 +91,8 @@ def main():
                         default='../../dist',
                         help='Path to TileDB installation directory. You can '
                              'also set the TILEDB_PATH environment variable.')
+    parser.add_argument('-l', '--list', action='store_true', default=False,
+                        help='List all available benchmarks and exit.')
     args = parser.parse_args()
 
     if find_tiledb_path(args) is None:
@@ -66,6 +101,11 @@ def main():
         sys.exit(1)
 
     build_benchmark(args)
+
+    if args.list:
+        list_benchmarks(show=True)
+        sys.exit(0)
+
     run_benchmark(args)
 
 
